3006295c1f95c618ff8900b6356ad71e50b46a6b
ilovemath25 <147607832+ilovemath25@users.noreply.github.com>
2024-04-06 14:57:33 +0800

    Update transition.js

--- script/transition.js
+++ script/transition.js
@@ -23,7 +23,7 @@ function home_transition(menu,index,link){
    nav.style.animation = "project_trans 1s";
    nav.style.cursor = "default";
    nav.innerHTML = "";
-   window.setTimeout(function(){window.history.back();},300);
+   window.setTimeout(function(){window.location.href = '';},300);
    window.reload(true);
 }
 function reload(){
